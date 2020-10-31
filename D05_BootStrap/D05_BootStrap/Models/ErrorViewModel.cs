75001cef8c80f3c34b61b7cfc428be76b3f7dcf0
namdongvando <32663464+namdongvando@users.noreply.github.com>
2020-10-31 14:24:08 +0700

    Init

---
+++ D05_BootStrap/D05_BootStrap/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace D05_BootStrap.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
