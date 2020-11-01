37b6ec35214b67ca76579bcb87f33aa6ded1a7e7
namdongvando <32663464+namdongvando@users.noreply.github.com>
2020-11-01 10:28:26 +0700

    Done Calculator

---
+++ D06_MVCBasic/D06_MVCBasic/Controllers/CalculatorController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D06_MVCBasic.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculate(double SoHang1, double SoHang2, string PhepToan)
+        {
+            double ketQua = 0;
+            switch(PhepToan)
+            {
+                case "+": ketQua = SoHang1 + SoHang2; break;
+                case "-": ketQua = SoHang1 - SoHang2; break;
+                case "*": ketQua = SoHang1 * SoHang2; break;
+                case "/": ketQua = SoHang1 / SoHang2; break;
+                case "%": ketQua = SoHang1 % SoHang2; break;
+                case "^": ketQua = Math.Pow(SoHang1, SoHang2); break;
+            }
+
+            ViewBag.A = SoHang1;
+            ViewBag.B = SoHang2;
+            ViewBag.PhepToan = PhepToan;
+            ViewBag.KQ = ketQua;
+            return View("Index");
+            //return Content($"{SoHang1} {PhepToan} {SoHang2} = {ketQua}");
+        }
+    }
+}
---
+++ D06_MVCBasic/D06_MVCBasic/Controllers/DemoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D06_MVCBasic.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/")]
+        [Route("ABC")]
+        public int A()
+        {
+            return new Random().Next();
+        }
+
+        [Route("xo-so/so-may-man")]
+        [Route("[controller]/xo-so")]
+        public int AX()
+        {
+            return new Random().Next(100, 1000);
+        }
+
+        [Route("{loai}/{tensp}")]
+        public string XuLy(string loai, string tensp)
+        {
+            return $"{loai}: {tensp}";
+        }
+
+    }
+}
