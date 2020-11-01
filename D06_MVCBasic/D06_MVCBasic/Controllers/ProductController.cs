ee2bc56bb0aba6a7ad14964c5d269f2975ae8dc0
namdongvando <32663464+namdongvando@users.noreply.github.com>
2020-11-01 11:49:58 +0700

    Done CRUD Product

---
+++ D06_MVCBasic/D06_MVCBasic/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using D06_MVCBasic.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D06_MVCBasic.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> _products = new List<Product>();
+
+        public IActionResult Index()
+        {
+            return View(_products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if(ModelState.IsValid)
+            {
+                _products.Add(product);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int masp)
+        {
+            var sp = _products.SingleOrDefault(p => p.Id == masp);
+            if(sp != null)//nếu có
+            {
+                return View(sp);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            var sp = _products.SingleOrDefault(p => p.Id == product.Id);
+            if (sp != null)//nếu có
+            {
+                sp.Name = product.Name;
+                sp.Price = product.Price;
+                sp.Quantity = product.Quantity;
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ D06_MVCBasic/D06_MVCBasic/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D06_MVCBasic.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
