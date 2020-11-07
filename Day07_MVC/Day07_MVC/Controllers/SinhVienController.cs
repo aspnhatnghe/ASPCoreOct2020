43fa5203187c6e9a06f8c007c561d4a4deec3a24
namdongvando <32663464+namdongvando@users.noreply.github.com>
2020-11-07 17:27:59 +0700

    Read Write File Text/JSON

---
+++ Day07_MVC/Day07_MVC/Controllers/SinhVienController.cs
@@ -0,0 +1,56 @@
+﻿using Day07_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Day07_MVC.Controllers
+{
+    public class SinhVienController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private string fileText = "datasv.txt";
+        private string fileJson = "datasv.json";
+
+        [HttpPost]
+        public IActionResult Manage(SinhVien sv, string GhiFile)
+        {
+            if (GhiFile == "Ghi file JSON")
+            {
+                var jsonSv = JsonConvert.SerializeObject(sv);
+                var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "JsonData", fileJson);
+
+                System.IO.File.WriteAllText(fullPath, jsonSv);
+                var desktopUrl = @"C:\Users\NN\Desktop\aaa.json";
+                System.IO.File.WriteAllText(desktopUrl, jsonSv);
+            }
+            else
+            {
+                //ghi file text
+                var data = new string[]
+                {
+                    sv.MaSV.ToString(),
+                    sv.HoTen,
+                    sv.Diem.ToString()
+                };
+                var fullPathText = Path.Combine(Directory.GetCurrentDirectory(), "Data", fileText);
+
+                System.IO.File.WriteAllLines(fullPathText, data);
+            }
+            return View("Index");
+        }
+
+        public IActionResult DocJson()
+        {
+            var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "JsonData", fileJson);
+            var content = System.IO.File.ReadAllText(fullPath);
+
+            var sv = JsonConvert.DeserializeObject<SinhVien>(content);
+
+            return View("Index", sv);
+        }
+    }
+}
---
+++ Day07_MVC/Day07_MVC/Models/SinhVien.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Day07_MVC.Models
+{
+    public class SinhVien
+    {
+        [Display(Name ="Mã SV")]
+        public int MaSV { get; set; }
+        [Display(Name ="Họ tên")]
+        public string HoTen { get; set; }
+        [Display(Name ="Điểm")]
+        public double Diem { get; set; }
+        [Display(Name = "Xếp loại")]
+        public string XepLoai
+        {
+            get
+            {
+                if (Diem < 5) return "Yếu";
+                if (Diem < 7) return "Trung bình";
+                if (Diem < 8.5) return "Khá";
+                return "Giỏi";
+            }
+        }
+    }
+}
