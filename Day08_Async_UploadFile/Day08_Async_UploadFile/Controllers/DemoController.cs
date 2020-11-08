53e9ed8dad984636c6ce63cd9fcc34888d7ef1f0
namdongvando <32663464+namdongvando@users.noreply.github.com>
2020-11-08 10:12:25 +0700

    Done multiple Upload

--- Day08_Async_UploadFile/Day08_Async_UploadFile/Controllers/DemoController.cs
+++ Day08_Async_UploadFile/Day08_Async_UploadFile/Controllers/DemoController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 using Day08_Async_UploadFile.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Day08_Async_UploadFile.Controllers
@@ -21,7 +23,7 @@ namespace Day08_Async_UploadFile.Controllers
 
             return $"Chạy hết {stopWatch.ElapsedMilliseconds}ms";
         }
-        
+
         public async Task<string> DemoABC()
         {
             var demo = new Demo();
@@ -34,6 +36,46 @@ namespace Day08_Async_UploadFile.Controllers
             stopWatch.Stop();
 
             return $"Chạy hết {stopWatch.ElapsedMilliseconds}ms";
+        }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
+        public IActionResult SingleFile(IFormFile MyFile)
+        {
+            if (MyFile != null)
+            {
+                var fileName = $"{DateTime.Now.Ticks}{MyFile.FileName}";
+                var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "HinhAnh", MyFile.FileName);
+                using (var file = new FileStream(fullPath, FileMode.Create))
+                {
+                    MyFile.CopyTo(file);
+                    ViewBag.ThongBao = "Upload thành công";
+                }
+            }
+            else
+            {
+                ViewBag.ThongBao = "Upload thất bại";
+            }
+            return View("Upload");
+        }
+
+        public IActionResult MultipleFile(List<IFormFile> MyFile)
+        {
+            if (MyFile != null)
+            {
+                foreach(var my_file in MyFile)
+                {
+                    var fileName = $"{DateTime.Now.Ticks}{my_file.FileName}";
+                    var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "HinhAnh", fileName);
+                    using (var file = new FileStream(fullPath, FileMode.Create))
+                    {
+                        my_file.CopyTo(file);
+                    }
+                }
+            }
+            return View("Upload");
         }
     }
 }
---
+++ Day08_Async_UploadFile/Day08_Async_UploadFile/Models/HangHoa.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Day08_Async_UploadFile.Models
+{
+    public class HangHoa
+    {
+        [Display(Name = "Mã số")]
+        public int MaHh { get; set; }
+
+        [Display(Name = "Tên hàng hóa")]
+        public string TenHh { get; set; }
+        [Display(Name = "Hình")]
+        public string Hinh { get; set; }
+
+        [Display(Name = "Đơn giá")]
+        public double DonGia { get; set; }
+        [Display(Name = "Số lượng")]
+        public int SoLuong { get; set; }
+        [Display(Name = "Thành tiền")]
+        public double ThanhTien => DonGia * SoLuong;
+    }
+}
