f06b31dfff07caaefe067d0e851ade9b2987ba31
namdongvando <32663464+namdongvando@users.noreply.github.com>
2020-11-14 13:38:38 +0700

    Day09 - Init

---
+++ Day09_Validation/Day09_Validation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Day09_Validation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
