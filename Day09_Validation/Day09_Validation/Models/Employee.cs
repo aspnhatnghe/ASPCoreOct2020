2b5b9000d7f2b677a1ad5312203a0f9978ab7e72
Hien Luong <hyhien@gmail.com>
2020-11-14 13:58:53 +0700

    Employee Model

---
+++ Day09_Validation/Day09_Validation/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Day09_Validation.Models
+{
+    public class Employee
+    {
+        public Guid? Id { get; set; }
+        [Display(Name = "Mã nhân viên")]
+        public string EmployeeId { get; set; }
+
+        [Display(Name = "Họ tên")]
+        [Required(ErrorMessage = "*")]
+        [MaxLength(150, ErrorMessage = "Tối đa 150 kí tự")]
+        public string FullName { get; set; }
+
+        [EmailAddress]        
+        public string Email { get; set; }
+        
+        [Url]
+        public string Website { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name ="Lương")]
+        public double Salary { get; set; }
+    }
+}
