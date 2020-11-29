1d6713dd88581918865d936b68aec257445d6458
Hien Luong <hyhien@gmail.com>
2020-11-29 10:26:35 +0700

    Done AddLoai

---
+++ Day13_ADONET/Day13_ADONET/Controllers/LoaiController.cs
@@ -0,0 +1,60 @@
+﻿using Day13_ADONET.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Day13_ADONET.Controllers
+{
+    public class LoaiController : Controller
+    {
+        private LoaiDataAccessLayer loaiDal;
+
+        public LoaiController()
+        {
+            loaiDal = new LoaiDataAccessLayer();
+        }
+
+        public IActionResult Index()
+        {
+            return View(loaiDal.GetAll());
+        }
+
+        public IActionResult Update(int id)
+        {
+            var loai = loaiDal.GetLoai(id);
+            return View(loai);
+        }
+
+        #region Tạo mới
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Loai loai, IFormFile Hinh)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Hinh != null)
+                {
+                    var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Hinh", "Loai", Hinh.FileName);
+                    using (var file = new FileStream(fullPath, FileMode.Create))
+                    {
+                        Hinh.CopyTo(file);
+                    }
+                    //cập nhật field Hình
+                    loai.Hinh = Hinh.FileName;
+                }
+
+                var maLoai = loaiDal.AddLoai(loai);
+                if (maLoai.HasValue)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View();
+        }
+        #endregion
+    }
+}
