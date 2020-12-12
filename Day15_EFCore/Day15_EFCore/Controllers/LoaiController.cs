272e9075e31fcf5194fa8acec85acbb88e53b8d3
HIENLTH <hyhien@gmail.com>
2020-12-12 15:30:21 +0700

    Add Loai

---
+++ Day15_EFCore/Day15_EFCore/Controllers/LoaiController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Day15_EFCore.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day15_EFCore.Controllers
+{
+    public class LoaiController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public LoaiController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Loais.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Loai loai)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(loai);
+                _context.SaveChanges();
+                return Redirect("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Day15_EFCore/Day15_EFCore/Migrations/20201212074355_DbInit.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Day15_EFCore.Migrations
+{
+    public partial class DbInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Loai",
+                columns: table => new
+                {
+                    MaLoai = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TenLoai = table.Column<string>(maxLength: 100, nullable: false),
+                    MoTa = table.Column<string>(nullable: true),
+                    Hinh = table.Column<string>(maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Loai", x => x.MaLoai);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HangHoa",
+                columns: table => new
+                {
+                    MaHh = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TenHh = table.Column<string>(maxLength: 100, nullable: false),
+                    Hinh = table.Column<string>(nullable: true),
+                    DonGia = table.Column<double>(nullable: false),
+                    SoLuongTon = table.Column<int>(nullable: false),
+                    MaLoai = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HangHoa", x => x.MaHh);
+                    table.ForeignKey(
+                        name: "FK_HangHoa_Loai_MaLoai",
+                        column: x => x.MaLoai,
+                        principalTable: "Loai",
+                        principalColumn: "MaLoai",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HangHoa_MaLoai",
+                table: "HangHoa",
+                column: "MaLoai");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HangHoa");
+
+            migrationBuilder.DropTable(
+                name: "Loai");
+        }
+    }
+}
