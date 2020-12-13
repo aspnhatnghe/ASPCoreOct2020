8fb01216f33f30c003d8d6b7b14ff0076121b8c7
HIENLTH <hyhien@gmail.com>
2020-12-13 08:40:27 +0700

    Project Init

---
+++ Day16_EFCore_DBFirst/Day16_EFCore_DBFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Day16_EFCore_DBFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
