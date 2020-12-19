756e354369a2573fdb3c0e2a680a0587b1781a62
NN <hyhien@gmail.com>
2020-12-19 17:30:03 +0700

    Ajax

--- Day16_EFCore_DBFirst/Day16_EFCore_DBFirst/Controllers/AjaxController.cs
+++ Day16_EFCore_DBFirst/Day16_EFCore_DBFirst/Controllers/AjaxController.cs
@@ -1,10 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Day16_EFCore_DBFirst.Entities;
+using Day16_EFCore_DBFirst.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Linq;
 
 namespace Day16_EFCore_DBFirst.Controllers
 {
     public class AjaxController : Controller
     {
+        private readonly eStore20Context _context;
+        public AjaxController(eStore20Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Search(string Keyword)
+        {
+            var data = _context.HangHoa.AsQueryable();
+            if(!string.IsNullOrEmpty(Keyword))
+            {
+                data = data.Where(hh => hh.TenHh.Contains(Keyword));
+            }
+
+            var dsHangHoa = data.Select(hh => new HangHoaAjaxVM { 
+                TenHh = hh.TenHh, MaHh = hh.MaHh,
+                DonGia = hh.DonGia.Value,
+                Hinh = hh.Hinh
+            });
+
+            return PartialView("PartialHangHoa", dsHangHoa);
+        }
+
         public IActionResult Index()
         {
             return View();
---
+++ Day16_EFCore_DBFirst/Day16_EFCore_DBFirst/ViewModels/HangHoaAjaxVM.cs
@@ -0,0 +1,10 @@
+﻿namespace Day16_EFCore_DBFirst.ViewModels
+{
+    public class HangHoaAjaxVM
+    {
+        public int MaHh { get; set; }
+        public string TenHh { get; set; }
+        public string Hinh { get; set; }
+        public double DonGia { get; set; }
+    }
+}
