7d0a03a864299bb8fd1190632bdec6e120657f01
NN <hyhien@gmail.com>
2020-12-20 09:16:11 +0700

    Demo AJAX return JSON

--- Day16_EFCore_DBFirst/Day16_EFCore_DBFirst/Controllers/AjaxController.cs
+++ Day16_EFCore_DBFirst/Day16_EFCore_DBFirst/Controllers/AjaxController.cs
@@ -24,13 +24,15 @@ namespace Day16_EFCore_DBFirst.Controllers
         public IActionResult Search(string Keyword)
         {
             var data = _context.HangHoa.AsQueryable();
-            if(!string.IsNullOrEmpty(Keyword))
+            if (!string.IsNullOrEmpty(Keyword))
             {
                 data = data.Where(hh => hh.TenHh.Contains(Keyword));
             }
 
-            var dsHangHoa = data.Select(hh => new HangHoaAjaxVM { 
-                TenHh = hh.TenHh, MaHh = hh.MaHh,
+            var dsHangHoa = data.Select(hh => new HangHoaAjaxVM
+            {
+                TenHh = hh.TenHh,
+                MaHh = hh.MaHh,
                 DonGia = hh.DonGia.Value,
                 Hinh = hh.Hinh
             });
@@ -47,6 +49,40 @@ namespace Day16_EFCore_DBFirst.Controllers
         public string ServerTime()
         {
             return DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+        }
+
+        [HttpGet]
+        public IActionResult JsonSearch()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult JsonSearch(string Keyword, double? From, double? To)
+        {
+            var data = _context.HangHoa.AsQueryable();
+            if (!string.IsNullOrEmpty(Keyword))
+            {
+                data = data.Where(hh => hh.TenHh.Contains(Keyword));
+            }
+            if (From.HasValue)
+            {
+                data = data.Where(hh => hh.DonGia >= From);
+            }
+            if (To.HasValue)
+            {
+                data = data.Where(hh => hh.DonGia <= To);
+            }
+
+            var dsHangHoa = data.Select(hh => new
+            {
+                hh.MaHh,
+                hh.TenHh,
+                hh.DonGia,
+                hh.MaLoaiNavigation.TenLoai
+            });
+
+            return Json(dsHangHoa);
         }
     }
 }
