2d80f3e4da12a492551c64fbef807e070eddc437
NN <hyhien@gmail.com>
2020-12-26 15:16:21 +0700

    Demo Authentication (default)

---
+++ Day19_AuthorAuthen/DemoAuthen/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAuthen.Controllers
+{
+    [Authorize]
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ABC()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult XYZ()
+        {
+            return View();
+        }
+    }
+}
---
+++ Day19_AuthorAuthen/DemoAuthen/Data/Loai.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAuthen.Data
+{
+    [Table("Loai")]
+    public class Loai
+    {
+        [Key]
+        public int MaLoai { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string TenLoai { get; set; }
+    }
+}
