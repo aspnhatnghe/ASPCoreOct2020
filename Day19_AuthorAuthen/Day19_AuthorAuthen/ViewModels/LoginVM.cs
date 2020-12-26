355182d872ed7cfec877ac4069cae352dbf285d7
NN <hyhien@gmail.com>
2020-12-26 16:53:11 +0700

    Authentication

--- Day19_AuthorAuthen/Day19_AuthorAuthen/ViewModels/LoginVM.cs
+++ Day19_AuthorAuthen/Day19_AuthorAuthen/ViewModels/LoginVM.cs
@@ -1,11 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
+using System.ComponentModel.DataAnnotations;
 
 namespace Day19_AuthorAuthen.ViewModels
 {
     public class LoginVM
     {
+        [Display(Name = "Tên đăng nhập")]
+        [Required]
+        public string Username { get; set; }
+
+        [Display(Name = "Mật khẩu")]
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
     }
 }
