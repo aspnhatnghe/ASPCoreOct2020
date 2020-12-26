d0aac4fcf801f95972686d312153b5302a675535
NN <hyhien@gmail.com>
2020-12-26 15:31:49 +0700

    AddDbContext

--- Day19_AuthorAuthen/Day19_AuthorAuthen/Startup.cs
+++ Day19_AuthorAuthen/Day19_AuthorAuthen/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Day19_AuthorAuthen.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,12 @@ namespace Day19_AuthorAuthen
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<eStore20Context>(option => {
+                option.UseSqlServer(Configuration.GetConnectionString("MyDatabase"));
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
