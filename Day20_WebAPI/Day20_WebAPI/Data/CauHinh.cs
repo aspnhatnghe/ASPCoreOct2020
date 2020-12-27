b76556f6ab4a502001e195cb1dfe204b75f108b0
NN <aspcorenn@gmail.com>
2020-12-27 08:17:07 +0700

    WebAPI init

---
+++ Day20_WebAPI/Day20_WebAPI/Data/CauHinh.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day20_WebAPI.Data
+{
+    public partial class CauHinh
+    {
+        public int? SoSanPham1Trang { get; set; }
+        public int? SoTrang { get; set; }
+        public int? SoNgayHetHanQc { get; set; }
+    }
+}
